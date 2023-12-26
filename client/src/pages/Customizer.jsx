29bfe024e33ddd7a6368b9a2b2d4d306672ec616
sincerelyyyash <yta0088@gmail.com>
2023-12-26 16:11:58 +0530

    Implemented download button

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -123,6 +123,13 @@ try{
                         />
                             ))}
                         {generateTabContent()}
+                        <button className='download-btn' onClick={downloadCanvasToImage}>
+        <img
+            src={download}
+            alt='download_image'
+            className='w-3/5 h-3/5 object-contain'
+        />
+    </button>
                     </div>
                 </div>
 
